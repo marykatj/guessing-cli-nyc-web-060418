3f1af73ce69ae00bfb2d00a3e5f2dcff39b3cfc6
Mary-Kate James <marykatj@gmail.com>
2018-05-23 16:41:07 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,4 +1,11 @@
 input = gets.chomp
 
+
 def run_guessing_game(input)
+  random = Math.floor(Math.random() * Math.floor(max))
+  if input == exit
+    break
+  elsif input == random
+    return "You guessed the correct number!"
+  else "The computer guessed #{random}."
 end
